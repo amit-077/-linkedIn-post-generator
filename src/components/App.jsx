af0afa206ad1ceb8ce6b2b3083d52b2fe19c05c1
amit_077 <amitkamat918@gmail.com>
2024-03-14 22:58:58 +0530

    bcd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,6 +21,8 @@ const generateAnswer = async (prompt) => {
 
 const App = () => {
   console.log(process.env.REACT_APP_API_KEY);
+  console.log(process.env);
+
   const [inputPost, setInputPost] = useState("");
   const [copiedOutput, setCopiedOutput] = useState("");
   // const [inputLength, setInputLength] = useState(0);
